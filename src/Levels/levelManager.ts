b5f199c0a7c384172376ccbb780c41ae8796187a
Jaco Brink <Jaco.Brink@derivco.co.za>
2019-07-16 17:03:23 +0200

    feat: level manager with some todos

---
+++ src/Levels/levelManager.ts
@@ -0,0 +1,9 @@
+export class LevelManager {
+  constructor() {
+    // TODO:
+    // Load all levels from a file
+    // Expose and set the current level
+    // Set milestones for level completed (E.g. enemies killed, reached end)
+    // Implement next level function
+  }
+}
