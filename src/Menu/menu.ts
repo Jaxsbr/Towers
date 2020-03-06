a4274ae3e327ef7bf5c69c37deb7c12115ec6c98
jaxsbr <jbrink386@gmail.com>
2020-03-07 10:09:44 +1300

    refactor image stage selection

--- src/Menu/menu.ts
+++ src/Menu/menu.ts
@@ -1,12 +1,10 @@
 import { GameScene } from "../Scenes/gameScene";
+import { ImageKeyPair } from "../Types/imageKeyPair";
 
 export class Menu {
-
     public stagedTowerImageWidth: number = 48;
     public stagedTowerImageHeight: number = 48;
-
-    private plainTowerImage: HTMLImageElement;
-    private slowTowerImage: HTMLImageElement;
+    private imageDictionary: ImageKeyPair[];
     private gameScene: GameScene;
     private stagedTower: string;    
     private menuHeight: number;
@@ -17,8 +15,10 @@ export class Menu {
         window.addEventListener('slowTowerClicked', () => { this.towerClicked("slow"); } );
         this.menuHeight = document.getElementById('tower_menu').clientHeight;
 
-        this.plainTowerImage = this.gameScene.game.assetManager.getImage("towerplain");
-        this.slowTowerImage = this.gameScene.game.assetManager.getImage("towerslow");
+        this.imageDictionary = [
+            { key: 'plain', image: this.gameScene.game.assetManager.getImage("towerplain") },
+            { key: 'slow', image: this.gameScene.game.assetManager.getImage("towerslow") }
+        ];
     }
 
     public clearStagedTower(): void {
@@ -31,28 +31,12 @@ export class Menu {
     }    
 
     public draw(): void {
-        // TODO: Refactor this, code being duplicated here when only image changes        
-
-        // TODO: Fix staged image render layout
-        // Behavoir: The staged tower renders at the mouse pointer y and x + 1/2. 
-        //           On clicking, the tower can be placed on an adjacent tile to the expected tile.
-        // Fix: Render the staged tower image's center over the mouse pointer.
-
-        if (!this.gameScene.mouseInfo) { return; }
+        if (!this.gameScene.mouseInfo || !this.stagedTower) { return; }
         
         const tX = this.gameScene.mouseInfo.x - (this.stagedTowerImageWidth / 2);
         const tY = this.gameScene.mouseInfo.y - (this.stagedTowerImageHeight / 2) - this.menuHeight;
 
-        switch (this.stagedTower) {
-            case 'plain':
-                    this.gameScene.renderEngine.renderImage(this.plainTowerImage, tX, tY, this.stagedTowerImageWidth, this.stagedTowerImageHeight);
-                break;
-            case 'slow':
-                    this.gameScene.renderEngine.renderImage(this.slowTowerImage, this.gameScene.mouseInfo.x, this.gameScene.mouseInfo.y, this.stagedTowerImageWidth, this.stagedTowerImageHeight);
-                break;
-            default:
-                    
-                break;
-        }        
+        const image = this.imageDictionary.find(x => x.key == this.stagedTower).image;
+        this.gameScene.renderEngine.renderImage(image, tX, tY, this.stagedTowerImageWidth, this.stagedTowerImageHeight);       
     }
 }
---
+++ src/Types/imageKeyPair.ts
@@ -0,0 +1,4 @@
+export type ImageKeyPair = {
+    key: string;
+    image: HTMLImageElement;
+}
