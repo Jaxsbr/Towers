ae4d5370924eb83763d6dbc5010de086b014b2f6
Jaco Brink <jaco.brink@xero.com>
2022-03-19 15:13:17 +1300

    added tower selection info pane

--- src/Towers/towerManager.ts
+++ src/Towers/towerManager.ts
@@ -43,13 +43,41 @@ export class TowerManager {
     }
 
     private selectTower(mouseRect: Rectangle): void {
+        let visibleInfo = false;
+        let selectedTower: BaseTower = null;
+
         for (let i = 0; i < this.towers.length; i += 1) {
             const tower = this.towers[i];
             if (tower.destinationTile.bounds.containsRect(mouseRect)) {
                 tower.setSelection(true);
+                visibleInfo = true;
+                selectedTower = tower;
             } else {
                 tower.setSelection(false);
             }
+        }
+
+        TowerManager.setSelectionInfo(visibleInfo, selectedTower);
+    }
+
+    static setSelectionInfo(visible: boolean, selectedTower: BaseTower): void {
+        const infoPane = document.getElementById('info_pane');
+
+        if (visible) {
+            infoPane.classList.remove('hidden');
+        } else {
+            infoPane.classList.add('hidden');
+            return;
+        }
+
+        if (selectedTower) {
+            const infoImg = document.getElementById('info_img') as HTMLImageElement;
+            // TODO: Once more tower types are added, use type to find correct tower image.
+            infoImg.src = window.assetManager.getImage('towerplain').src;
+
+            // TODO: Once more tower types are added, use type to find correct tower text.
+            const infoText = document.getElementById('info_text');
+            infoText.innerText = 'Plain Tower';
         }
     }
 }
